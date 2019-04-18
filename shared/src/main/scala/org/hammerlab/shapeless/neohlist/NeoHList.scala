0a345808f56c32345b21b8bbb485b03c8c107d49
Ryan Williams <ryan.blake.williams@gmail.com>
2019-04-18 21:45:57 +0000

    NeoHList checkpoint

--- shared/src/main/scala/org/hammerlab/shapeless/neohlist/NeoHList.scala
+++ shared/src/main/scala/org/hammerlab/shapeless/neohlist/NeoHList.scala
@@ -18,7 +18,15 @@ sealed trait OHList[+L <: HList]
 
 object OHList {
   case object nil extends OHList[HNil]
-  case class Cons[+H, +L <: HList](h: Option[H], t: OHList[L]) extends OHList[H :: L]
+  sealed trait Cons[+H, +L <: HList] extends OHList[H :: L] {
+    def h_? : Option[H]
+    def t: OHList[L]
+  }
+  object Cons {
+    def apply[H, L <: HList](h: Option[H], t: OHList[L]): Cons[H, L] = Base(h, t)
+    def unapply[H, L <: HList](cons: Cons[H, L]): Option[(Option[H], OHList[L])] = Some((cons.h_?, cons.t))
+    private case class Base[+H, +L <: HList](h_? : Option[H], t: OHList[L]) extends Cons[H, L]
+  }
 
   sealed trait Empty[L <: HList] {
     def apply(): OHList[L]
@@ -29,9 +37,25 @@ object OHList {
   }
 
   def empty[L <: HList](implicit e: Empty[L]): OHList[L] = e()
+
+  implicit val hnil: Generic.Aux[OHList[HNil], HNil] = new Generic[OHList[HNil]] {
+    type Repr = HNil
+    def to(t: OHList[HNil]): HNil = HNil
+    def from(r: HNil): OHList[HNil] = nil
+  }
+  implicit def cons[H, L <: HList, OL <: HList](implicit g: Lazy[Generic.Aux[OHList[L], OL]]): Generic.Aux[OHList[H :: L], Option[H] :: OL] =
+    new Generic[OHList[H :: L]] {
+      type Repr = Option[H] :: OL
+      def to(t: OHList[H :: L]): Repr = t match {
+        case Cons(h, t) ⇒ h :: g.value.to(t)
+      }
+      def from(r: Repr): OHList[H :: L] = r match {
+        case h :: t ⇒ Cons(h, g.value.from(t))
+      }
+    }
 }
 
-sealed trait NeoHList[+H, +L <: HList] /*extends OHList[H :: L]*/ {
+sealed trait NeoHList[H, L <: HList] extends OHList[H :: L] {
   //type H_? <: Option[Head]
   //def head: H_?
   //def head: Option[Head]
@@ -44,18 +68,37 @@ sealed trait NeoHList[+H, +L <: HList] /*extends OHList[H :: L]*/ {
   //type NonEmptyTail <: NeoHList
 }
 object NeoHList {
-//  case class Base[H](h: H) extends NeoHList[H, HNil] {
-//    def head = Some(h)
-//  }
 
-//  def apply[H](h: H): Cons[H, HNil] = new Cons(h, OHList.nil)
-//  def apply[H, L <: HList](h: H, t: OHList[L]): Cons[H, L] = Cons(h, t)
-//  def apply[H1, H2, L <: HList](t: NeoHList[H2, L]): Extend[H1, H2, L] = Extend(t)
+  def apply[H](h: H): Cons[H, HNil] = new Cons(h, OHList.nil)
+  def apply[H, L <: HList](h: H, t: OHList[L]): Cons[H, L] = Cons(h, t)
+  def apply[H1, H2, L <: HList](t: NeoHList[H2, L]): Extend[H1, H2, L] = Extend(t)
 
-  case class Cons[+H, +L <: HList] (h: H, t: OHList[L]) extends NeoHList[H, L]
-//  object Cons {
-//    def make[H](h: H): Cons[H, HNil] = new Cons(h, OHList.nil)
-    //def unapply[H, L <: HList](c: Cons[H, L]): Option[(H, OHList[L])] = Some((c.h, c.t))
-//  }
-  case class Extend[+H1, +H2, +L <: HList](t: NeoHList[H2, L]) extends NeoHList[H1, H2 :: L]
+  case class Cons[H, L <: HList] (h: H, t: OHList[L]) extends NeoHList[H, L] with OHList.Cons[H, L] {
+    def h_? = Some(h)
+  }
+  object Cons {
+    def apply[H](h: H): Cons[H, HNil] = new Cons(h, OHList.nil)
+//    def unapply[H, L <: HList](c: Cons[H, L]): Option[(H, OHList[L])] = Some((c.h, c.t))
+  }
+  case class Extend[H1, H2, L <: HList](t: NeoHList[H2, L]) extends NeoHList[H1, H2 :: L]
+
+  implicit def genBase[H]: Generic.Aux[NeoHList[H, HNil], Cons[H, HNil] :+: CNil] =
+    new Generic[NeoHList[H, HNil]] {
+      type Repr = Cons[H, HNil] :+: CNil
+      def to(t: NeoHList[H, HNil]): Repr = t match { case c: Cons[H, HNil] ⇒ Inl(c) }
+      def from(r: Repr): NeoHList[H, HNil] = r match { case Inl(r) ⇒ r }
+    }
+
+  implicit def genCons[H1, H2, L <: HList]: Generic.Aux[NeoHList[H1, H2 :: L], Cons[H1, H2 :: L] :+: Extend[H1, H2, L] :+: CNil] =
+    new Generic[NeoHList[H1, H2 :: L]] {
+      type Repr = Cons[H1, H2 :: L] :+: Extend[H1, H2, L] :+: CNil
+      def to(t: NeoHList[H1, H2 :: L]): Repr = t match {
+        case l: Cons[H1, H2 :: L] ⇒ Inl(l)
+        case l: Extend[H1, H2, L] ⇒ Inr(Inl(l))
+      }
+      def from(r: Repr): NeoHList[H1, H2 :: L] = r match {
+        case Inl(cons) ⇒ cons
+        case Inr(Inl(extend)) ⇒ extend
+      }
+    }
 }
--- shared/src/test/scala/org/hammerlab/shapeless/neohlist/Test.scala
+++ shared/src/test/scala/org/hammerlab/shapeless/neohlist/Test.scala
@@ -2,12 +2,57 @@ package org.hammerlab.shapeless.neohlist
 
 import shapeless._
 
+sealed trait I[H, L <: HList]
+object I {
+  //type I[H, L <: HList]
+  case class A[H, L <: HList](h: H, t: OHList[L]) extends I[H, L]
+  //def extend[H1, H2, L <: HList]: I[H1, H2 :: L]
+  case class B[H1, H2, L <: HList](t: I[H2, L]) extends I[H1, H2 :: L]
+
+  implicit def genA[H]: Generic.Aux[I[H, HNil], A[H, HNil] :+: CNil] =
+    new Generic[I[H, HNil]] {
+      type Repr = A[H, HNil] :+: CNil
+      def to(t: I[H, HNil]): Repr = t match {
+        case a: A[H, HNil] ⇒ Inl(a)
+      }
+      def from(r: Repr): I[H, HNil] =
+        r match {
+          case Inl(a) ⇒ a
+        }
+    }
+  implicit def genB[H1, H2, L <: HList]: Generic.Aux[I[H1, H2 :: L], A[H1, H2 :: L] :+: B[H1, H2, L] :+: CNil] =
+    new Generic[I[H1, H2 :: L]] {
+      type Repr = A[H1, H2 :: L] :+: B[H1, H2, L] :+: CNil
+      override def to(t: I[H1, H2 :: L]): Repr = t match {
+        case a: A[H1, H2 :: L] ⇒ Inl(a)
+        case b: B[H1, H2, L] ⇒ Inr(Inl(b))
+      }
+      override def from(r: Repr): I[H1, H2 :: L] = r match {
+        case Inl(a) ⇒ a
+        case Inr(Inl(b)) ⇒ b
+      }
+    }
+}
+
 class Test
   extends hammerlab.Suite {
   test("generics") {
+    import I.{A, B}
+    !![Generic[A[String, HNil]]]
+    !![Generic[B[String, Int, HNil]]]
+    !![Generic[I[String, HNil]]]
+    !![Generic[I[String, Int :: HNil]]]
+
     import NeoHList.{ Cons, Extend }
     !![Generic[Cons[String, HNil]]]
     !![Generic[Extend[String, Int, HNil]]]
     !![Generic[NeoHList[String, Int :: HNil]]]
+    !![Generic[NeoHList[String, HNil]]]
+
+    !![Generic[OHList[HNil]]]
+    !![Generic[OHList[String :: HNil]]]
+
+//    !![Generic[String :: Int :: HNil]]
+//    !![Generic[String :: OHList[HNil] :: HNil]]
   }
 }
